ef10ae87a29e59ae32480bc62af12212e558e9f5
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-01-09 21:40:07 +0100

    Refactored

--- sv_common.php
+++ sv_common.php
@@ -14,47 +14,50 @@ class sv_common extends init {
 	public function __construct() {
 
 	}
-	public function init(){
-		// CSS
-		$css_bootstrap							= static::$scripts->create( $this );
-		$css_bootstrap
-			->set_ID('bootstrap')
-			->set_source($this->get_file_url('lib/css/bootstrap.min.css'), $this->get_file_path('lib/css/bootstrap.min.css'))
-			->set_inline(true);
+
+	public function init() {
+		// Module Info
+		$this->set_module_title( 'SV Common' );
+		$this->set_module_desc( __( 'This module manages general styles, scripts & dependencies.', $this->get_module_name() ) );
+
+		// Loads Styles
+		$css_bootstrap = static::$scripts->create( $this )
+			->set_ID( 'bootstrap' )
+			->set_source( $this->get_file_url( 'lib/css/bootstrap.min.css' ), $this->get_file_path( 'lib/css/bootstrap.min.css' ) )
+			->set_inline( true );
 		
-		$css_default							= static::$scripts->create( $this );
-		$css_default
-			->set_ID('default')
-			->set_source($this->get_file_url('lib/css/default.css'), $this->get_file_path('lib/css/default.css'))
-			->set_deps(array($css_bootstrap->get_handle()))
-			->set_inline(true);
-		
-		// JS
-		if (!is_admin()){
-			add_action("wp_enqueue_scripts", array($this, 'wp_enqueue_scripts'), 11);
+		static::$scripts->create( $this )
+			->set_ID( 'default' )
+			->set_source( $this->get_file_url( 'lib/css/default.css' ), $this->get_file_path( 'lib/css/default.css' ) )
+			->set_deps( array( $css_bootstrap->get_handle() ) )
+			->set_inline( true );
+
+		// Loads Scripts
+		if ( ! is_admin() ) {
+			add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ), 11 );
 		}
 		
-		$js_jquery							= static::$scripts->create( $this );
-		$js_jquery
-			->set_ID('jquery')
-			->set_type('js')
-			->set_no_prefix(true)
-			->set_source($this->get_file_url('lib/js/jquery-3.3.1.min.js'), $this->get_file_path('lib/js/jquery-3.3.1.min.js'));
+		$js_jquery = static::$scripts->create( $this )
+			->set_ID( 'jquery' )
+			->set_type( 'js' )
+			->set_no_prefix( true )
+			->set_source( $this->get_file_url( 'lib/js/jquery-3.3.1.min.js' ), $this->get_file_path( 'lib/js/jquery-3.3.1.min.js' ) );
 		
-		$js_bootstrap							= static::$scripts->create( $this );
-		$js_bootstrap
-			->set_ID('bootstrap')
-			->set_type('js')
-			->set_source($this->get_file_url('lib/js/bootstrap.bundle.min.js'), $this->get_file_path('lib/js/bootstrap.bundle.min.js'))
-			->set_deps(array($js_jquery->get_handle()));
+		static::$scripts->create( $this )
+			->set_ID( 'bootstrap' )
+			->set_type( 'js' )
+			->set_source( $this->get_file_url( 'lib/js/bootstrap.bundle.min.js' ), $this->get_file_path( 'lib/js/bootstrap.bundle.min.js' ) )
+			->set_deps( array( $js_jquery->get_handle() ) );
 		
 		// Action Hooks
 		add_action( 'after_setup_theme', array( $this, 'after_setup_theme' ) );
 		add_action( 'wp_footer', array( $this, 'wp_footer' ), 999999 );
 	}
-	public function wp_enqueue_scripts(){
+
+	public function wp_enqueue_scripts() {
 		wp_deregister_script('jquery');
 	}
+
 	public function after_setup_theme() {
 		load_theme_textdomain( 'sv_100', get_template_directory() . '/lib/lang' );
 
@@ -70,6 +73,6 @@ class sv_common extends init {
 	public function wp_footer() {
 		$theme = wp_get_theme();
 
-		echo "\n\n" . '<!-- This website uses the ' . $theme->get('Name') . ' Theme by ' . $theme->get('Author') . ' (' . $theme->get('AuthorURI') . ') -->' . "\n\n";
+		echo "\n\n" . '<!-- This website uses the ' . $theme->get( 'Name' ) . ' Theme by ' . $theme->get( 'Author' ) . ' (' . $theme->get( 'AuthorURI' ) . ') -->' . "\n\n";
 	}
 }
