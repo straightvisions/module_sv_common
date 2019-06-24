6624bb2fa9c1418ba2b85d4c1fcfd41cd6a68bc6
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-06-24 21:42:53 +0200

    Replaced sub titles

--- lib/backend/tpl/settings.php
+++ lib/backend/tpl/settings.php
@@ -2,14 +2,14 @@
 if(current_user_can('activate_plugins')):
 	?>
 	<div class="sv_section_description"><?php echo $module->get_section_desc(); ?></div>
-	<h4>Font</h4>
+	<h3 class="divider">Font</h3>
 	<div class="sv_setting_flex">
 		<?php echo $module->get_settings()['font_family']->run_type()->form(); ?>
 		<?php echo $module->get_settings()['font_size']->run_type()->form(); ?>
 		<?php echo $module->get_settings()['font_color']->run_type()->form(); ?>
 		<?php echo $module->get_settings()['font_line_height']->run_type()->form(); ?>
 	</div>
-	<h4>Background</h4>
+	<h3 class="divider">Background</h3>
 	<div>
 		<div class="sv_setting_flex">
 			<?php echo $module->get_settings()['background_color']->run_type()->form(); ?>
@@ -23,7 +23,7 @@ if(current_user_can('activate_plugins')):
 			<?php echo $module->get_settings()['background_image_attachment']->run_type()->form(); ?>
 		</div>
 	</div>
-	<h4>Selection</h4>
+	<h3 class="divider">Selection</h3>
 	<div class="sv_setting_flex">
 		<?php echo $module->get_settings()['selection_color']->run_type()->form(); ?>
 		<?php echo $module->get_settings()['selection_color_background']->run_type()->form(); ?>
