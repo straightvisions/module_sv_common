41b19902d2c68f7501bba80183549eb567256b07
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-06-28 15:59:27 +0200

    Cleaned code for better maintainability

--- lib/backend/tpl/settings.php
+++ lib/backend/tpl/settings.php
@@ -1,31 +1,41 @@
 <?php
-if(current_user_can('activate_plugins')):
+	if ( current_user_can( 'activate_plugins' ) ) {
 	?>
 	<div class="sv_section_description"><?php echo $module->get_section_desc(); ?></div>
 	<h3 class="divider">Font</h3>
 	<div class="sv_setting_flex">
-		<?php echo $module->get_settings()['font_family']->run_type()->form(); ?>
-		<?php echo $module->get_settings()['font_size']->run_type()->form(); ?>
-		<?php echo $module->get_settings()['font_color']->run_type()->form(); ?>
-		<?php echo $module->get_settings()['font_line_height']->run_type()->form(); ?>
+		<?php
+			echo $module->get_settings()['font_family']->run_type()->form();
+			echo $module->get_settings()['font_size']->run_type()->form();
+			echo $module->get_settings()['font_color']->run_type()->form();
+			echo $module->get_settings()['font_line_height']->run_type()->form();
+		?>
 	</div>
 	<h3 class="divider">Background</h3>
 	<div>
 		<div class="sv_setting_flex">
-			<?php echo $module->get_settings()['background_color']->run_type()->form(); ?>
-			<?php echo $module->get_settings()['background_image']->run_type()->form(); ?>
-			<?php echo $module->get_settings()['background_image_media_size']->run_type()->form(); ?>
+			<?php
+				echo $module->get_settings()['background_color']->run_type()->form();
+				echo $module->get_settings()['background_image']->run_type()->form();
+				echo $module->get_settings()['background_image_media_size']->run_type()->form();
+			?>
 		</div>
 		<div class="sv_setting_flex">
-			<?php echo $module->get_settings()['background_image_position']->run_type()->form(); ?>
-			<?php echo $module->get_settings()['background_image_size']->run_type()->form(); ?>
-			<?php echo $module->get_settings()['background_image_repeat']->run_type()->form(); ?>
-			<?php echo $module->get_settings()['background_image_attachment']->run_type()->form(); ?>
+			<?php
+				echo $module->get_settings()['background_image_position']->run_type()->form();
+				echo $module->get_settings()['background_image_size']->run_type()->form();
+				echo $module->get_settings()['background_image_repeat']->run_type()->form();
+				echo $module->get_settings()['background_image_attachment']->run_type()->form();
+			?>
 		</div>
 	</div>
 	<h3 class="divider">Selection</h3>
 	<div class="sv_setting_flex">
-		<?php echo $module->get_settings()['selection_color']->run_type()->form(); ?>
-		<?php echo $module->get_settings()['selection_color_background']->run_type()->form(); ?>
+		<?php
+			echo $module->get_settings()['selection_color']->run_type()->form();
+			echo $module->get_settings()['selection_color_background']->run_type()->form();
+		?>
 	</div>
-<?php endif; ?>
+	<?php
+	}
+?>
--- sv_common.php
+++ sv_common.php
@@ -21,162 +21,169 @@ class sv_common extends init {
 		$this->set_section_title( __( 'Common', 'sv100' ) )
 			->set_section_desc( __( 'Base settings for the whole frontend.', 'sv100' ) )
 			->set_section_type( 'settings' )
-			->set_section_template_path($this->get_path('lib/backend/tpl/settings.php'));
+			->set_section_template_path( $this->get_path( 'lib/backend/tpl/settings.php' ) );
 
-		$this->get_root()->add_section($this);
+		$this->get_root()->add_section( $this );
 
 		$this->load_settings()->register_scripts();
 
 		// Action Hooks
 		add_action( 'after_setup_theme', array( $this, 'after_setup_theme' ) );
 	}
+	
 	protected function register_scripts(): sv_common {
 		$this->scripts_queue['frontend'] =
 			static::$scripts->create( $this )
-				->set_ID( 'frontend' )
-				->set_path( 'lib/frontend/css/default.css' )
-				->set_inline(true)
-				->set_is_enqueued();
+							->set_ID( 'frontend' )
+							->set_path( 'lib/frontend/css/default.css' )
+							->set_inline( true )
+							->set_is_enqueued();
 
 		return $this;
 	}
+	
 	public function load_settings(): sv_common{
-		$fonts			= array('' => __('choose...', 'sv100'));
-		$font_array 	=  $this->get_module('sv_webfontloader')->get_setting('fonts')->run_type()->get_data();
+		$fonts			= array( '' => __( 'choose...', 'sv100' ) );
+		$font_array 	= $this->get_module( 'sv_webfontloader' )->get_setting( 'fonts' )->run_type()->get_data();
 
 		if ( $font_array ) {
 			foreach( $font_array as $font ) {
-				$fonts[$font['entry_label']]		= $font['entry_label'];
+				$fonts[ $font['entry_label'] ]		= $font['entry_label'];
 			}
 		}
 
 		$this->s['font_family'] =
 			$this->get_setting()
-				->set_ID('font_family')
-				->set_title(__('Font Family', 'sv100'))
-				->set_description(__('Base font for all frontend elements.', 'sv100'))
-				->load_type('select')
-				->set_options($fonts);
+				 ->set_ID( 'font_family' )
+				 ->set_title( __( 'Font Family', 'sv100' ) )
+				 ->set_description( __( 'Base font for all frontend elements.', 'sv100' ) )
+				 ->load_type( 'select' )
+				 ->set_options( $fonts );
 
 		$this->s['font_size'] =
 			$this->get_setting()
-				->set_ID('font_size')
-				->set_title(__('Font Size', 'sv100'))
-				->set_description(__('Default Font Size in Pixel', 'sv100'))
-				->set_default_value(16)
-				->load_type('number');
+				 ->set_ID( 'font_size' )
+				 ->set_title( __( 'Font Size', 'sv100' ) )
+				 ->set_description( __( 'Default Font Size in Pixel', 'sv100' ) )
+				 ->set_default_value( 16 )
+				 ->load_type( 'number' );
 
 		$this->s['font_color'] =
 			$this->get_setting()
-				->set_ID('font_color')
-				->set_title(__('Font Color', 'sv100'))
-				->set_description(__('Default Font Color', 'sv100'))
-				->set_default_value('#000000')
-				->load_type('color');
+				 ->set_ID( 'font_color' )
+				 ->set_title( __( 'Font Color', 'sv100' ) )
+				 ->set_description( __( 'Default Font Color', 'sv100' ) )
+				 ->set_default_value( '#000000' )
+				 ->load_type( 'color' );
 
 		$this->s['font_line_height'] =
 			$this->get_setting()
-				->set_ID('font_line_height')
-				->set_title(__('Font Line Height', 'sv100'))
-				->set_description(__('Default Line Height in Pixel', 'sv100'))
-				->set_default_value(23)
-				->load_type('number');
+				 ->set_ID( 'font_line_height' )
+				 ->set_title( __( 'Font Line Height', 'sv100' ) )
+				 ->set_description( __( 'Default Line Height in Pixel', 'sv100' ) )
+				 ->set_default_value( 23 )
+				 ->load_type( 'number' );
 
 		$this->s['background_color'] =
 			$this->get_setting()
-				->set_ID('background_color')
-				->set_title(__('Background Color', 'sv100'))
-				->set_description(__('Background Color for Body', 'sv100'))
-				->set_default_value('#FFFFFF')
-				->load_type('color');
+				 ->set_ID( 'background_color' )
+				 ->set_title( __( 'Background Color', 'sv100' ) )
+				 ->set_description( __( 'Background Color for Body', 'sv100' ) )
+				 ->set_default_value( '#FFFFFF' )
+				 ->load_type( 'color' );
 
 		$this->s['background_image'] =
 			$this->get_setting()
-				->set_ID('background_image')
-				->set_title(__('Background Image', 'sv100'))
-				->set_description(__('Background Image for Body', 'sv100'))
-				->load_type('upload');
+				 ->set_ID( 'background_image' )
+				 ->set_title( __('Background Image', 'sv100' ) )
+				 ->set_description( __( 'Background Image for Body', 'sv100' ) )
+				 ->load_type( 'upload' );
 
 		$this->s['background_image_media_size'] =
 			$this->get_setting()
-				->set_ID('background_image_media_size')
-				->set_title(__('Background Image Media Size', 'sv100'))
-				->set_description(__('Background Image Media Size for Body', 'sv100'))
-				->set_default_value('large')
-				->load_type('select')
-				->set_options(array_combine(get_intermediate_image_sizes(), get_intermediate_image_sizes()));
+				 ->set_ID( 'background_image_media_size' )
+				 ->set_title( __( 'Background Image Media Size', 'sv100' ) )
+				 ->set_description( __( 'Background Image Media Size for Body', 'sv100' ) )
+				 ->set_default_value( 'large' )
+				 ->load_type( 'select' )
+				 ->set_options( array_combine( get_intermediate_image_sizes(), get_intermediate_image_sizes() ) );
 
 		$this->s['background_image_position'] =
 			$this->get_setting()
-				->set_ID('background_image_position')
-				->set_title(__('Background Position', 'sv100'))
-				->set_description(__('Background Image Position Value', 'sv100'))
-				->set_placeholder('center top')
-				->set_default_value('center top')
-				->load_type('text');
+				 ->set_ID( 'background_image_position' )
+				 ->set_title( __( 'Background Position', 'sv100' ) )
+				 ->set_description( __( 'Background Image Position Value', 'sv100' ) )
+				 ->set_placeholder( 'center top' )
+				 ->set_default_value( 'center top' )
+				 ->load_type( 'text' );
 
 		$this->s['background_image_size'] =
 			$this->get_setting()
-				->set_ID('background_image_size')
-				->set_title(__('Background Size', 'sv100'))
-				->set_description(__('Background Image Size Value', 'sv100'))
-				->set_placeholder('cover')
-				->set_default_value('cover')
-				->load_type('text');
+				 ->set_ID( 'background_image_size' )
+				 ->set_title( __( 'Background Size', 'sv100' ) )
+				 ->set_description( __( 'Background Image Size Value', 'sv100' ) )
+				 ->set_placeholder( 'cover' )
+				 ->set_default_value( 'cover' )
+				 ->load_type( 'text' );
 
 		$this->s['background_image_repeat'] =
 			$this->get_setting()
-				->set_ID('background_image_repeat')
-				->set_title(__('Background Repeat', 'sv100'))
-				->set_description(__('Background Image Repeat', 'sv100'))
-				->set_default_value('no-repeat')
-				->load_type('select')
-				->set_options(array(
-					'' => __('choose...', 'sv100'),
-					'repeat' => 'repeat',
-					'repeat-x' => 'repeat-x',
-					'repeat-y' => 'repeat-y',
-					'no-repeat' => 'no-repeat',
-					'space' => 'space',
-					'round' => 'round',
-					'initial' => 'initial',
-					'inherit' => 'inherit'
-				));
+				 ->set_ID( 'background_image_repeat' )
+				 ->set_title( __( 'Background Repeat', 'sv100' ) )
+				 ->set_description( __( 'Background Image Repeat', 'sv100' ) )
+				 ->set_default_value( 'no-repeat' )
+				 ->load_type( 'select' )
+				 ->set_options(
+					array(
+						'' 			=> __( 'choose...', 'sv100' ),
+						'repeat' 	=> 'repeat',
+						'repeat-x' 	=> 'repeat-x',
+						'repeat-y' 	=> 'repeat-y',
+						'no-repeat' => 'no-repeat',
+						'space' 	=> 'space',
+						'round' 	=> 'round',
+						'initial' 	=> 'initial',
+						'inherit' 	=> 'inherit'
+					)
+				);
 
 		$this->s['background_image_attachment'] =
 			$this->get_setting()
-				->set_ID('background_image_attachment')
-				->set_title(__('Background Attachment', 'sv100'))
-				->set_description(__('Background Image Attachment', 'sv100'))
-				->set_default_value('fixed')
-				->load_type('select')
-				->set_options(array(
-					'' => __('choose...', 'sv100'),
-					'scroll' => 'scroll',
-					'fixed' => 'fixed',
-					'local' => 'local',
-					'initial' => 'initial',
-					'inherit' => 'inherit'
-				));
+				 ->set_ID( 'background_image_attachment' )
+				 ->set_title( __( 'Background Attachment', 'sv100' ) )
+				 ->set_description( __( 'Background Image Attachment', 'sv100' ) )
+				 ->set_default_value( 'fixed' )
+				 ->load_type( 'select' )
+				 ->set_options(
+					array(
+						'' 			=> __('choose...', 'sv100'),
+						'scroll' 	=> 'scroll',
+						'fixed' 	=> 'fixed',
+						'local' 	=> 'local',
+						'initial' 	=> 'initial',
+						'inherit' 	=> 'inherit'
+					)
+				);
 
 		$this->s['selection_color'] =
 			$this->get_setting()
-				->set_ID('selection_color')
-				->set_title(__('Selection Color', 'sv100'))
-				->set_description(__('Color of selected text', 'sv100'))
-				->set_default_value('#FFFFFF')
-				->load_type('color');
+				 ->set_ID( 'selection_color' )
+				 ->set_title( __( 'Selection Color', 'sv100' ) )
+				 ->set_description( __( 'Color of selected text', 'sv100' ) )
+				 ->set_default_value( '#FFFFFF' )
+				 ->load_type( 'color' );
 
 		$this->s['selection_color_background'] =
 			$this->get_setting()
-				->set_ID('selection_color_background')
-				->set_title(__('Selection Background Color', 'sv100'))
-				->set_description(__('Background color of selected text', 'sv100'))
-				->set_default_value('#779a76')
-				->load_type('color');
+				 ->set_ID( 'selection_color_background' )
+				 ->set_title( __( 'Selection Background Color', 'sv100' ) )
+				 ->set_description( __( 'Background color of selected text', 'sv100' ) )
+				 ->set_default_value( '#779a76' )
+				 ->load_type( 'color' );
 
 		return $this;
 	}
+	
 	public function after_setup_theme() {
 		add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'title-tag' );
