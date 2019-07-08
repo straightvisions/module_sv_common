51aaa57e166e1401fbc7ca163fa379e112a5da29
Matthias Bathke <matthias-bathke@straightvisions.com>
2019-07-08 20:09:27 +0200

    REFACTOR CSS VARS

--- lib/backend/tpl/settings.php
+++ lib/backend/tpl/settings.php
@@ -3,42 +3,16 @@
 	?>
 	<div class="sv_section_description"><?php echo $module->get_section_desc(); ?></div>
 
-		<h3 class="divider"><?php _e( 'General', 'sv100' ); ?></h3>
-		<div class="sv_setting_flex">
-			<?php
-				echo $module->get_settings()['padding']->run_type()->form();
-			?>
-		</div>
-	
-	<h3 class="divider"><?php _e( 'Font', 'sv100' ); ?></h3>
-	<div class="sv_setting_flex">
-		<?php
-			echo $module->get_settings()['font_family']->run_type()->form();
-			echo $module->get_settings()['font_size']->run_type()->form();
-			echo $module->get_settings()['font_color']->run_type()->form();
-			echo $module->get_settings()['font_line_height']->run_type()->form();
-		?>
-	</div>
-
-	<h3 class="divider"><?php _e( 'Background', 'sv100' ); ?></h3>
-	<div>
-		<div class="sv_setting_flex">
-			<?php
-				echo $module->get_settings()['background_color']->run_type()->form();
-				echo $module->get_settings()['background_image']->run_type()->form();
-				echo $module->get_settings()['background_image_media_size']->run_type()->form();
-			?>
-		</div>
-		<div class="sv_setting_flex">
-			<?php
-				echo $module->get_settings()['background_image_position']->run_type()->form();
-				echo $module->get_settings()['background_image_size']->run_type()->form();
-				echo $module->get_settings()['background_image_repeat']->run_type()->form();
-				echo $module->get_settings()['background_image_attachment']->run_type()->form();
-			?>
-		</div>
-	</div>
-
+    <h3 class="divider"><?php _e( 'General', 'sv100' ); ?></h3>
+    <div class="sv_setting_flex">
+        <?php
+            echo $module->get_settings()['padding']->run_type()->form();
+        ?>
+    </div>
+    <?php
+        include($module->get_root()->get_path('core_theme/lib/backend/tpl/settings_draft_font.php'));
+		include($module->get_root()->get_path('core_theme/lib/backend/tpl/settings_draft_background.php'));
+    ?>
 	<h3 class="divider"><?php _e( 'Selection', 'sv100' ); ?></h3>
 	<div class="sv_setting_flex">
 		<?php
