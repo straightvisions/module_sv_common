463e2f40dbd5103b7b36cc96bee813e280898bea
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-07-10 20:25:48 +0200

    Removed Settings Draft Feature and replaced it with the Settings Components Feature

--- lib/frontend/css/config.php
+++ lib/frontend/css/config.php
@@ -1,28 +1,33 @@
 <?php
-	$font_family					= $script->get_parent()->get_setting( 'font_family' )->run_type()->get_data();
+	// Text Settings
+	$font_family				= $script->get_parent()->get_setting( 'font_family' )->run_type()->get_data();
+	
 	if ( $font_family ) {
-		$font						= $script->get_parent()->get_module( 'sv_webfontloader' )->get_font_by_label( $font_family );
+		$font					= $script->get_parent()->get_module( 'sv_webfontloader' )->get_font_by_label( $font_family );
 	} else {
-		$font                       = false;
+		$font                     = false;
 	 }
 
-	$font_size						= $script->get_parent()->get_setting( 'font_size' )->run_type()->get_data();
-	$font_color						= $script->get_parent()->get_setting( 'font_color' )->run_type()->get_data();
-	$font_line_height				= $script->get_parent()->get_setting( 'font_line_height' )->run_type()->get_data();
-
-	$background_color				= $script->get_parent()->get_setting( 'background_color' )->run_type()->get_data();
-	$background_image				= $script->get_parent()->get_setting( 'background_image' )->run_type()->get_data();
-	$background_image_media_size	= $script->get_parent()->get_setting( 'background_image_media_size' )->run_type()->get_data();
-	$background_image_position		= $script->get_parent()->get_setting( 'background_image_position' )->run_type()->get_data();
-	$background_image_size			= $script->get_parent()->get_setting( 'background_image_size' )->run_type()->get_data();
-	$background_image_repeat		= $script->get_parent()->get_setting( 'background_image_repeat' )->run_type()->get_data();
-	$background_image_attachment	= $script->get_parent()->get_setting( 'background_image_attachment' )->run_type()->get_data();
-
-	$selection_color				= $script->get_parent()->get_setting( 'selection_color' )->run_type()->get_data();
-	$selection_color_background		= $script->get_parent()->get_setting( 'selection_color_background' )->run_type()->get_data();
+	$font_size					= $script->get_parent()->get_setting( 'font_size' )->run_type()->get_data();
+	$text_color					= $script->get_parent()->get_setting( 'text_color' )->run_type()->get_data();
+	$line_height				= $script->get_parent()->get_setting( 'line_height' )->run_type()->get_data();
+	
+	// Background Settings
+	$bg_color					= $script->get_parent()->get_setting( 'bg_color' )->run_type()->get_data();
+	$bg_image					= $script->get_parent()->get_setting( 'bg_image' )->run_type()->get_data();
+	$bg_media_size				= $script->get_parent()->get_setting( 'bg_media_size' )->run_type()->get_data();
+	$bg_position				= $script->get_parent()->get_setting( 'bg_position' )->run_type()->get_data();
+	$bg_size					= $script->get_parent()->get_setting( 'bg_size' )->run_type()->get_data();
+	$bg_fit						= $script->get_parent()->get_setting( 'bg_fit' )->run_type()->get_data();
+	$bg_repeat					= $script->get_parent()->get_setting( 'bg_repeat' )->run_type()->get_data();
+	$bg_attachment				= $script->get_parent()->get_setting( 'bg_attachment' )->run_type()->get_data();
+	
+	// Selection Settings
+	$selection_color			= $script->get_parent()->get_setting( 'selection_color' )->run_type()->get_data();
+	$selection_color_bg			= $script->get_parent()->get_setting( 'selection_color_background' )->run_type()->get_data();
 ?>
 
-	 /* Global Vars */
+/* Global Vars */
 :root {
 	--sv100_sv_common-padding: <?php echo $script->get_parent()->get_setting( 'padding' )->run_type()->get_data() . 'px' ?>;
 	--sv100_sv_common-max-width-lg: 1300px;
@@ -40,7 +45,7 @@
 }
 
 *::selection {
-	background-color: <?php echo $selection_color_background; ?>;
+	background-color: <?php echo $selection_color_bg; ?>;
 	color: <?php echo $selection_color; ?>;
 }
 
@@ -48,22 +53,25 @@ html, body {
 	margin: 0;
 	padding: 0;
 	overflow-x: hidden;
+	font-size: <?php echo $font_size; ?>px;
 }
 
 body {
 	font-family: <?php echo ( $font ? '"' . $font['family'] . '", ' : '' ); ?>sans-serif;
 	font-weight: <?php echo ( $font ? '"' . $font['weight'] . '", ' : '400' ); ?>;
-	font-size: <?php echo $font_size; ?>px;
-	color: <?php echo $font_color; ?>;
-	line-height: <?php echo $font_line_height; ?>px;
-	background-color: <?php echo $background_color; ?>;
+	color: <?php echo $text_color; ?>;
+	line-height: <?php echo $line_height; ?>px;
+	background-color: <?php echo $bg_color; ?>;
 
-	<?php if ( $background_image ) { ?>
-		background-image: url( '<?php echo wp_get_attachment_image_src( $background_image, $background_image_media_size )[0]; ?>' );
-		background-position:<?php echo $background_image_position; ?>;
-		background-size:<?php echo $background_image_size; ?>;
-		background-repeat:<?php echo $background_image_repeat; ?>;
-		background-attachment:<?php echo $background_image_attachment; ?>;
+	<?php
+		if ( $bg_image ) {
+			$bg_size = $bg_size > 0 ? $bg_size . 'px' : $bg_fit;
+			?>
+		background-image: url( '<?php echo wp_get_attachment_image_src( $bg_image, $bg_media_size )[0]; ?>' );
+		background-position:<?php echo $bg_position; ?>;
+		background-size:<?php echo $bg_size; ?>;
+		background-repeat:<?php echo $bg_repeat; ?>;
+		background-attachment:<?php echo $bg_attachment; ?>;
 	<?php } ?>
 }
 
--- sv_common.php
+++ sv_common.php
@@ -38,13 +38,55 @@
 								->set_path( 'lib/frontend/css/config.php' )
 								->set_inline(true)
 								->set_is_enqueued();
+			
 	
 			return $this;
 		}
 		
 		public function load_settings(): sv_common{
-			$this->settings_draft_font()->settings_draft_background();
-	
+			// Text Settings
+			$this->get_settings_component( 'font_family','font_family' );
+			$this->get_settings_component( 'font_size','font_size' );
+			$this->get_settings_component( 'line_height','line_height' );
+			$this->get_settings_component( 'text_color','text_color' );
+			
+			/*
+			// Link Settings
+			$this->get_settings_component( 'font_family_link','font_family' );
+			$this->get_settings_component( 'text_color_link','text_color' );
+			$this->get_settings_component( 'text_deco_link','text_decoration' );
+			$this->get_settings_component( 'text_bg_link','background_color' );
+			$this->s['text_bg_active_link'] =
+				$this->get_setting()
+					 ->set_ID( 'text_bg_active_link' )
+					 ->set_title( __( 'Activate Background Color', 'sv100' ) )
+					 ->set_default_value( 0 )
+					 ->load_type( 'checkbox' );
+			
+			// Link Settings (Hover/Focus)
+			$this->get_settings_component( 'font_family_link_hover','font_family' );
+			$this->get_settings_component( 'text_color_link_hover','text_color' );
+			$this->get_settings_component( 'text_deco_link_hover','text_decoration' );
+			$this->get_settings_component( 'text_bg_link_hover','background_color' );
+			$this->s['text_bg_active_link_hover'] =
+				$this->get_setting()
+					 ->set_ID( 'text_bg_active_link_hover' )
+					 ->set_title( __( 'Activate Background Color', 'sv100' ) )
+					 ->set_default_value( 0 )
+					 ->load_type( 'checkbox' );
+			*/
+			
+			// Background Settings
+			$this->get_settings_component( 'bg_color','background_color' );
+			$this->get_settings_component( 'bg_image','background_image' );
+			$this->get_settings_component( 'bg_media_size','background_media_size' );
+			$this->get_settings_component( 'bg_position','background_position' );
+			$this->get_settings_component( 'bg_size','background_size' );
+			$this->get_settings_component( 'bg_fit','background_fit' );
+			$this->get_settings_component( 'bg_repeat','background_repeat' );
+			$this->get_settings_component( 'bg_attachment','background_attachment' );
+			
+			// Selection Settings
 			$this->s['selection_color'] =
 				$this->get_setting()
 					 ->set_ID( 'selection_color' )
