aa08506ca5e64d3db201a8a0a246e31d76a2fa79
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-07-26 16:06:38 +0200

    Added missing theme support

--- sv_common.php
+++ sv_common.php
@@ -95,6 +95,7 @@
 			add_theme_support( 'post-thumbnails' );
 			add_theme_support( 'title-tag' );
 			add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+			add_theme_support( 'automatic-feed-links' );
 			add_theme_support( 'custom-logo' );
 	
 			add_post_type_support( 'page', 'excerpt' );
