4a455582744aedc4895efb40a67e8c9bec15df25
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-09-21 19:35:44 +0200

    MIGRATE TO V5.100

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -3,20 +3,20 @@
 	echo $_s->build_css(
 		is_admin() ? '.edit-post-visual-editor.editor-styles-wrapper *' : '*', // we need to explicitly define that for form fields, too, to avoid that Chrome will override it with user agent style sheets.
 		array(
-			'-webkit-hyphens' => $script->get_parent()->get_setting('hyphens')->get_data(),
-			'-ms-hyphens' => $script->get_parent()->get_setting('hyphens')->get_data(),
-			'hyphens' => $script->get_parent()->get_setting('hyphens')->get_data()
+			'-webkit-hyphens' => $module->get_setting('hyphens')->get_data(),
+			'-ms-hyphens' => $module->get_setting('hyphens')->get_data(),
+			'hyphens' => $module->get_setting('hyphens')->get_data()
 		)
 	);
 
 	echo $_s->build_css(
 		is_admin() ? '.edit-post-visual-editor.editor-styles-wrapper' : 'body, button, input, select, textarea', // we need to explicitly define that for form fields, too, to avoid that Chrome will override it with user agent style sheets.
 		array_merge(
-			$script->get_parent()->get_setting('font')->get_css_data('font-family'),
-			$script->get_parent()->get_setting('font_size_normal')->get_css_data('font-size','','px'),
-			$script->get_parent()->get_setting('line_height')->get_css_data('line-height'),
-			$script->get_parent()->get_setting('text_color')->get_css_data(),
-			$script->get_parent()->get_setting('bg_color')->get_css_data('background-color')
+			$module->get_setting('font')->get_css_data('font-family'),
+			$module->get_setting('font_size_normal')->get_css_data('font-size','','px'),
+			$module->get_setting('line_height')->get_css_data('line-height'),
+			$module->get_setting('text_color')->get_css_data(),
+			$module->get_setting('bg_color')->get_css_data('background-color')
 		)
 	);
 
@@ -26,15 +26,15 @@
 		.editor-styles-wrapper a:visited'
 			: 'a, a:visited',
 		array_merge(
-			$script->get_parent()->get_setting('font_link')->get_css_data('font-family'),
-			$script->get_parent()->get_setting('text_color_link')->get_css_data()
+			$module->get_setting('font_link')->get_css_data('font-family'),
+			$module->get_setting('text_color_link')->get_css_data()
 		)
 	);
 
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper p:not(.has-text-color) a:hover, .editor-styles-wrapper p:not(.has-text-color) a:focus' : 'a:hover, a:focus',
 		array_merge(
-			$script->get_parent()->get_setting('text_color_link_hover')->get_css_data()
+			$module->get_setting('text_color_link_hover')->get_css_data()
 		)
 	);
 
@@ -42,7 +42,7 @@
 	// Text Decoration
 	$properties			= array();
 
-	$value				= $script->get_parent()->get_setting('text_deco_link')->get_data();
+	$value				= $module->get_setting('text_deco_link')->get_data();
 	if($value){
 		$imploded		= false;
 		foreach($value as $breakpoint => $val) {
@@ -74,7 +74,7 @@
 	// @todo doubled code
 	$properties			= array();
 
-	$value				= $script->get_parent()->get_setting('text_deco_link_hover')->get_data();
+	$value				= $module->get_setting('text_deco_link_hover')->get_data();
 
 	if($value){
 		$imploded		= false;
@@ -106,7 +106,7 @@
 	$properties					= array();
 
 	foreach($_s->get_parent()->get_editor_font_sizes() as $font_size){
-		$properties['font-size']		= $_s->prepare_css_property_responsive($script->get_parent()->get_setting( 'font_size_'. $font_size['slug'])->get_data(),'','px !important');
+		$properties['font-size']		= $_s->prepare_css_property_responsive($module->get_setting( 'font_size_'. $font_size['slug'])->get_data(),'','px !important');
 		echo $_s->build_css(
 			'.has-'.$font_size['slug'].'-font-size',
 			$properties
@@ -116,12 +116,12 @@
 
 /* Global Vars */
 :root {
---sv100_sv_common-max-width-alignfull: <?php echo $script->get_parent()->get_setting('max_width_alignfull')->get_data() ? $script->get_parent()->get_setting('max_width_alignfull')->get_data().'px' : '100vw'; ?>;
---sv100_sv_common-max-width-alignwide: <?php echo $script->get_parent()->get_setting('max_width_alignwide')->get_data(); ?>px;
---sv100_sv_common-max-width-text: <?php echo $script->get_parent()->get_setting('max_width_text')->get_data(); ?>px;
+--sv100_sv_common-max-width-alignfull: <?php echo $module->get_setting('max_width_alignfull')->get_data() ? $module->get_setting('max_width_alignfull')->get_data().'px' : '100vw'; ?>;
+--sv100_sv_common-max-width-alignwide: <?php echo $module->get_setting('max_width_alignwide')->get_data(); ?>px;
+--sv100_sv_common-max-width-text: <?php echo $module->get_setting('max_width_text')->get_data(); ?>px;
 }
 
 *::selection {
-background-color: rgba(<?php echo $script->get_parent()->get_setting('selection_color_background')->get_data(); ?>);
-color: rgba(<?php echo $script->get_parent()->get_setting('selection_color')->get_data(); ?>);
+background-color: rgba(<?php echo $module->get_setting('selection_color_background')->get_data(); ?>);
+color: rgba(<?php echo $module->get_setting('selection_color')->get_data(); ?>);
 }
