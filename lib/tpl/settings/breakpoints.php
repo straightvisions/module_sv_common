2a25741e94d3ec059012f219876551ffe6a5e73e
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-03-25 15:38:07 +0100

    REPLACE WHITESPACE WITH TABS

--- lib/tpl/settings/breakpoints.php
+++ lib/tpl/settings/breakpoints.php
@@ -1,25 +1,25 @@
 <div class="sv_setting_subpage">
 	<h2><?php _e('Breakpoints', 'sv100'); ?></h2>
-    <p><?php _e( 'Define the min-width for the different devices (in pixel).' ); ?></p>
+	<p><?php _e( 'Define the min-width for the different devices (in pixel).' ); ?></p>
 
-    <div class="sv_setting_flex">
+	<div class="sv_setting_flex">
 		<?php
 		echo $module->get_setting( 'breakpoint_mobile' )->form();
 		echo $module->get_setting( 'breakpoint_mobile_landscape' )->form();
 		?>
-    </div>
-    <div class="sv_setting_flex">
+	</div>
+	<div class="sv_setting_flex">
 		<?php
 		echo $module->get_setting( 'breakpoint_tablet' )->form();
 		echo $module->get_setting( 'breakpoint_tablet_landscape' )->form();
 		?>
-    </div>
-    <div class="sv_setting_flex">
+	</div>
+	<div class="sv_setting_flex">
 		<?php
 		echo $module->get_setting( 'breakpoint_tablet_pro' )->form();
 		echo $module->get_setting( 'breakpoint_tablet_pro_landscape' )->form();
 		?>
-    </div>
+	</div>
 	<div class="sv_setting_flex">
 		<?php
 			echo $module->get_setting( 'breakpoint_desktop' )->form();
