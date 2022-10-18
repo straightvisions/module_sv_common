969f1522fd1d4d2cfe88b5d2261a7c6b05284928
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-10-18 11:42:53 +0200

    fix missing setting form

--- lib/tpl/settings/general.php
+++ lib/tpl/settings/general.php
@@ -20,6 +20,7 @@
 	<div class="sv_setting_flex">
 		<?php
 		echo $module->get_setting( 'font' )->form();
+		echo $module->get_setting( 'font_size' )->form();
 		echo $module->get_setting( 'text_color' )->form();
 		echo $module->get_setting( 'line_height' )->form();
 		?>
